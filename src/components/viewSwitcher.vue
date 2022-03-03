888320e9adbb3c0b53b6ed2c8c9e1c97681491b7
kbarral333 <59743864+kbarral333@users.noreply.github.com>
2022-03-03 15:08:43 +0800

    minor change
    renamed label into "Show incomplete rows only"

--- src/components/viewSwitcher.vue
+++ src/components/viewSwitcher.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="input-group justify-content-center">
-      <label class="form-check-label" for="flexSwitchCheckDefault">Display all rows</label>
+      <label class="form-check-label" for="flexSwitchCheckDefault">Show incomplete rows only</label>
       <div class="form-switch">
         <input class="form-check-input" @change="$emit('switch',$event)" type="checkbox" value="" id="flexSwitchCheckDefault">
       </div>
