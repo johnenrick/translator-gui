55c788db57f2d90f33f26a31d56e32003de55738
kbarral333 <59743864+kbarral333@users.noreply.github.com>
2022-03-07 09:55:57 +0800

    improvement
    -reset moved to the leftmost side of the screen
    -added confirmation modal before when reset button is clicked

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,7 +5,7 @@
     </div>
     <div class="row standard-height">
       <div class="col">
-        <button class="btn btn-danger" @click="resetTranslator">Reset</button>
+        <button class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal">Reset</button>
       </div>
       <div class="col">
         <button @click="storeChanges" class="btn btn-info">Save</button>
@@ -89,6 +89,23 @@
       <div class="col">
         <div :hidden="isNotifyingPhrase" class="alert " :class="notifClass" role="alert">
           {{alertMessage}}
+        </div>
+      </div>
+    </div>
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Reset Table</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            Are you sure you want to reset the table?
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="button" @click="resetTranslator" data-bs-dismiss="modal" class="btn btn-primary">Save changes</button>
+          </div>
         </div>
       </div>
     </div>
